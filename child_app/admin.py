c954eb4172c7f9d6b6b8a7a2c5e8f83ef194c214
BigBoss2011 <chaoukibayoudhi@gmail.com>
2021-11-13 09:36:48 +0100

    Add inheritance

--- child_app/admin.py
+++ child_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Parent,Child,Place,ChildPlace
 # Register your models here.
+admin.site.register([Parent,Child,Place,ChildPlace])
+#admin.site.register(Parent)
+#admin.site.register(Parent)
